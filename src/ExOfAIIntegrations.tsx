5ec8bc58fb6d490a653af4fe901b4552828f00f3
nextgen-automation <nextgenai.automation@gmail.com>
2025-05-28 11:36:46 -0400

    Add scroll to top functionality

--- src/ExOfAIIntegrations.tsx
+++ src/ExOfAIIntegrations.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { ChevronRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import Header from './components/Header';
 
 const ExOfAIIntegrations: React.FC = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <motion.div 
       initial={{ opacity: 0 }}
